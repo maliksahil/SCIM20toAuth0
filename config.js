8d4b9114c083078691c6f727dc3b09a25efa7431
Leandro Pelorosso <leandro.pelorosso@729solutions.com>
2018-06-07 11:02:19 -0600

    - Updated postman templates. - Updated config and server.js so domain is not needed.

--- config.js
+++ config.js
@@ -4,11 +4,8 @@ var jwks = require('jwks-rsa');
 'use strict';
 
 module.exports = {
-    AUTH0_CLIENT_DOMAIN: 'DOMAIN',
-    AUTH0_MANAGEMENT_API_TOKEN: 'TOKEN',
-
-    AUTH0_TENANT: 'TENANT',
-    AUTH0_API_AUDIENCE: 'AUDIENCE',
-
-    AUTH0_HASHING_ALGORITHM: 'ALGORITHM'
+    AUTH0_TENANT: 'TENANT', // your <tenant>
+    AUTH0_MANAGEMENT_API_TOKEN: 'TOKEN', // grab this from Auth0 Management API https://manage.auth0.com/#/apis/xxx/test
+    AUTH0_API_AUDIENCE: 'AUDIENCE', // You'll need to setup a API, and define an audience(identifier) for it. This is that audience.
+    AUTH0_HASHING_ALGORITHM: 'ALGORITHM' // RS256
 };
