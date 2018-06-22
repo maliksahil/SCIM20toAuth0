5fa0a077674a31b2643dd40d3e674b9c52d36573
Amine El Haimer <amine.el.haimer@desjardins.com>
2018-06-22 11:01:37 -0400

    #noissue change to webtask

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
-var jwt = require('express-jwt');
-var jwks = require('jwks-rsa');
-
 'use strict';
 
 module.exports = {
-    AUTH0_TENANT: 'TENANT', // your <tenant>
-    AUTH0_MANAGEMENT_API_TOKEN: 'TOKEN', // grab this from Auth0 Management API https://manage.auth0.com/#/apis/xxx/test
-    AUTH0_API_AUDIENCE: 'AUDIENCE', // You'll need to setup a API, and define an audience(identifier) for it. This is that audience.
-    AUTH0_HASHING_ALGORITHM: 'ALGORITHM' // RS256
+    AUTH0_ISSUER_DOMAIN: '', // your <tenant>
+    AUTH0_CLIENT_ID: '', // grab this from Auth0 Management API https://manage.auth0.com/#/apis/xxx/test
+    AUTH0_CLIENT_SECRET: '', // You'll need to setup a API, and define an audience(identifier) for it. This is that audience.
+    AUDIENCE: '' // RS256
 };
--- server.js
+++ server.js
@@ -1,93 +1,31 @@
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var app         = express();
-var morgan      = require('morgan');
-var apiProxy    = require('./lib/api-proxy');
-var errors      = require('./lib/error-response');
-var config      = require('./config');
-var auth        = require('./lib/authentication');
-var schemas     = require('./lib/schemas')();
+const Express = require('express');
+const app = require('./lib');
+const config = require('./config');
+const morgan = require('morgan');
 
-var Auth0ManagementClient = require('auth0').ManagementClient;
-var auth0Client = new Auth0ManagementClient({
-  domain: config.AUTH0_TENANT + ".auth0.com",
-  token: config.AUTH0_MANAGEMENT_API_TOKEN
-});
+const server = Express();
+const port = process.env.PORT || 8081;
 
 // configure app
-app.use(morgan('dev')); // log requests to the console
+server.use(morgan('dev')); // log requests to the console
+server.use(mockWebtaskContext);
+server.use(app);
 
-// configure body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+function mockWebtaskContext(req, res, next) {
+    // Mock `req.webtaskContext` for standalone servers
+    if (!req.webtaskContext) {
+        req.webtaskContext = {
+            secrets: {
+                AUTH0_ISSUER_DOMAIN: config.AUTH0_ISSUER_DOMAIN,
+                AUTH0_CLIENT_ID: config.AUTH0_CLIENT_ID,
+                AUTH0_CLIENT_SECRET: config.AUTH0_CLIENT_SECRET,
+                AUDIENCE: config.AUDIENCE
+            }
+        };
+    }
+    next();
+}
 
-var port = process.env.PORT || 8081;
-var router = express.Router();
-
-/**  endpoints **/
-
-router.route('/users')
-  .post(function (req, res, next) {
-    var scimUser = req.body;
-    return apiProxy.createUser(auth0Client, scimUser, (err, newScimUser) => {
-          if (err) return next(err);
-          res.status(201).json(newScimUser);
-    });    
-  })
-  .get(function (req, res, next) {
-    return apiProxy.getUsers(auth0Client, req.query, (err, scimUsers) => {
-          if (err) return next(err);
-          res.status(200).json(scimUsers);
-    });
-  });
-
-router.route('/users/:user_id')
-  .get(function (req, res, next) {
-    req.query.user_id = req.params.user_id;
-    return apiProxy.getUser(auth0Client, req.query, (err, scimUser) => {
-      if (err) return next(err);
-      res.status(200).json(scimUser);
-    });    
-  })
-
-  .patch(function (req, res, next) {
-    var scimUser = req.body;
-    return apiProxy.updateUser(auth0Client, { id: req.params.user_id }, scimUser, (err, editedScimUser) => {
-      if (err) return next(err);
-      res.status(200).json(editedScimUser);
-    });
-  })
-
-  .delete(function (req, res, next) {
-    return apiProxy.deleteUser(auth0Client, { id: req.params.user_id }, (err) => {
-      if (err) return next(err); // TODO: If user does not exists, it should return 404 according to the SCIM specification, but auth0 API does not!
-      res.sendStatus(204); // 204:No-Content
-    });
-  });
-
-router.route('/schemas')
-  .get(function (req, res, next) {
-      res.status(200).json(schemas.all());
-  });
-
-router.route('/schemas/:schema_id')
-  .get(function (req, res, next) {
-      // if the required schema exists
-      var schema = schemas.byId(req.params.schema_id);
-      if(schema){
-        res.status(200).json(schema);
-      }else{ // return error indicting the schema is not found
-        res.status(404).send(errors.wrap({message:'Schema not found.', statusCode:404}));
-      }
-  });
-
-app.use(auth.jwtCheck, router);
-app.use('/scim', router);
-
-app.use(function (err, req, res, next) {
-  var status = (err.statusCode >= 100 && err.statusCode < 600 ? err.statusCode : 500) || 500;
-  res.status(status).send(errors.wrap(err));
+server.listen(port, function () {
+    console.log('Server started on port', port);
 });
-
-app.listen(port);
-console.log('Listening on port ' + port);
